52b556be447d7fcd13b9e477841685c212d6d1e4
fuad <nafisfuadisc@gmail.com>
2026-02-12 04:17:36 +0600

    basic controller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +11,8 @@ Route::get('/user', function (Request $request) {
 Route::get('/hello', function() {
     return ["message" => "Hello Laravel API!"];
 });
+
+Route::get('/posts', [PostController::class, 'index'])->name('post.index');
+Route::post('/posts', [PostController::class, 'store'])->name('post.store');
+
+Route::get('/posts/{id}', [PostController::class, 'show'])->name('post.show');
