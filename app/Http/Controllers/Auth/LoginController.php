dbc08a172e09260730c79fb3a737028c974f9d72
fuad <nafisfuadisc@gmail.com>
2026-02-15 08:31:36 +0600

    login/registration

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
@@ -13,25 +14,26 @@ class LoginController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): Response
+    public function store(LoginRequest $request)
     {
         $request->authenticate();
 
-        $request->session()->regenerate();
+        $user = $request->user();
+        $token = $user->createToken("main")->plainTextToken;
 
-        return response()->noContent();
+        return [
+            "user" => new UserResource($user),
+            "token" => $token,
+        ];
     }
 
     /**
-     * Destroy an authenticated session.
+     * Destroy an authenticated token.
      */
     public function destroy(Request $request): Response
     {
-        Auth::guard('web')->logout();
-
-        $request->session()->invalidate();
-
-        $request->session()->regenerateToken();
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
 
         return response()->noContent();
     }
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -34,7 +34,7 @@ class RegisterController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
+        // Auth::login($user);
 
         return response()->noContent();
     }
