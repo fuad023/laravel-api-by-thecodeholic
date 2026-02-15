0aba6b905ca4c3d4a454b382346064ad5d581936
fuad <nafisfuadisc@gmail.com>
2026-02-15 21:10:35 +0600

    rate limiting

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Api\V1\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware("auth:sanctum")->group(function () {
+Route::middleware(["auth:sanctum", "throttle:api"])->group(function () {
     Route::get("/user", function (Request $request) {
         return $request->user();
     });
